e8f0598976638637aed93cd65abcc7c76e48052d
yangyang <yangjoanne216@gmail.com>
2023-05-19 14:23:47 +0200

    版本六： 发送给老师的版本，规范了代码的注释

--- client.c
+++ client.c
@@ -1,13 +1,13 @@
 /*************************************************************
-File name : client.c
-Author : Ningxin YE, Yiqing CHEN, Yang YANG
-Class : L3 MIAGE(université paris Dauphine_PSL)
-Day : 2023/5/15
-Compile command line : gcc client.c -pthread -o client
-The command line for running the program ： ./client 172.20.10.5
-For stop the programme : quit
-
-ctrl + c met le programme en mode édition (les messages acceptés ne sont pas affichés pendant ce temps)
+* File name : client.c
+* Version 6
+* Author : Ningxin YE, Yiqing CHEN, Yang YANG
+* L3 MIAGE(université paris Dauphine_PSL)
+* Date : 2023/5/15
+* Compile command : gcc client.c -pthread -o client
+* Command to run the program : ./client 
+* puts the program into edit mode : ctrl + C
+* Terminate the program: quit
 **************************************************************/
 #include<stdio.h>
 #include<unistd.h>
@@ -18,27 +18,34 @@ ctrl + c met le programme en mode édition (les messages acceptés ne sont pas a
 #include<pthread.h>
 #include <sys/un.h> 
 
-#define MESSAGE_SIZE 1000
-int const connect_time_max=3;
-int edit_only_mode = 0;  // 标记是否处于写模式
-char pendingMessages[MESSAGE_SIZE][MESSAGE_SIZE];  // 存储待显示的消息
-int pendingMessagesCount = 0;  // 存储待显示消息的数量
-void handle_sigint(int sig);
-long client_socket = -1;  //变成全局变量便于访问
+#define MESSAGE_SIZE 1000 /*The length of the temporarily stored message*/
 
+int const connect_time_max=3; /*Maximum number of attempts to connect to the server*/
+
+/*0 means not in edit-only mode, 1 means in edit-only mode.
+Messages sent by other clients are not displayed while the client is in edit_only_mode
+ctrl + c -> enter the edit_only_mode, 
+sending a message -> exit edit_only_mode*/
+int edit_only_mode = 0;  
+char pendingMessages[MESSAGE_SIZE][MESSAGE_SIZE];  /*Temporary storage of messages received at edit_only_mode*/
+int pendingMessagesCount = 0;  /*Number of messages stored*/
+
+
+/* Declaration of functions */
 int creat_client_socket(long * client_socket);
 int  connect_serveur(int client_socket);
 void *client_recevie(void *socket);
+void handle_sigint(int sig);
+
 int main(int argc, char const *argv[])
 {
+    long client_socket = -1; 
     signal(SIGINT, handle_sigint); 
     pthread_t thread_client;
-    //const char *ip_adresse = argv[1];
     char *client_reponse;
-    //create socket error
+
     if(creat_client_socket(&client_socket)<0)
         return -1;
-     //connect error
     if(connect_serveur(client_socket) <0)
         return -1;
     
@@ -49,19 +56,18 @@ int main(int argc, char const *argv[])
     while(1){
         fgets(client_reponse,100,stdin);
         //scanf("%[^\n]",client_reponse);
-        //Remove line breaks from strings
+        /*Remove line breaks from strings*/
         client_reponse[strlen(client_reponse) - 1] = '\0';
         send(client_socket,client_reponse,strlen(client_reponse),0);
         if(strncmp(client_reponse, "quit", 4) == 0)
         {   
             printf("logged out！\n");
-            pthread_cancel(thread_client);  // 关闭接收线程
+            pthread_cancel(thread_client);  
             break;
         }
-        // 新增的代码开始
-        edit_only_mode = 0;  // 退出写模式
-
-        // 显示所有待显示的消息
+        /*Exit edit_only mode*/
+        edit_only_mode = 0;  
+        /*Show messages received during edit_only_mode*/
         if(pendingMessagesCount!=0)
         {
              printf("-----Exiting edit-only mode.--------\nThe following are all the messages you received during the edit only mode: \n");
@@ -69,26 +75,20 @@ int main(int argc, char const *argv[])
             {
                 printf("%s\n", pendingMessages[i]);
             }
-
-            // 清空待显示消息
             pendingMessagesCount = 0;
 
-        }
-       
+        }     
     }
     close(client_socket);
     return 0;
 }
 
-void handle_sigint(int sig) {
-   edit_only_mode = !edit_only_mode;  // 切换写模式
-
-    // 如果切换到写模式，显示提示信息
-    if (edit_only_mode) {
-        printf("-----Edit only mode (ctrl+c)--------\nenter your message: \n");
-    } 
-}
-
+/**
+* @function:        creat_client_socket
+* @brief            Create a unix client socket
+* @return           >0 a file descriptor pointing to socket 
+*                   <0 fail 
+*/
 int creat_client_socket(long * client_socket){
     *client_socket = socket(AF_UNIX,SOCK_STREAM,0);
     if(client_socket<0){
@@ -100,10 +100,16 @@ int creat_client_socket(long * client_socket){
     return 0;
 }
 
+/**
+* @fuction:     accept_client
+* @brief        Connect to the server
+* @param[in]    client_socket   The client's socket file descriptor
+* @return       0:success -1:fail
+*/
 int  connect_serveur(int client_socket){
     struct sockaddr_un serv_addr={0};
     serv_addr.sun_family = AF_UNIX;
-    strcpy(serv_addr.sun_path, "./MySock");	// Chemin vers fichier socket
+    strcpy(serv_addr.sun_path, "./MySock");	/*Chemin vers fichier socket*/
     int connect_time = 3;
 
     while(connect_time--){
@@ -123,35 +129,56 @@ int  connect_serveur(int client_socket){
         
     }
     }
-    return 0;
-    
+    return 0;   
 }
+
+/**
+ * @function:    client_receive
+ * @brief       Handles the reception of messages for a client
+ *              If a "SERVER_EXIT" message is received, it exits.
+ *              In edit_only_mode, the received messages are stored in a pending messages buffer; 
+ *              otherwise, they are printed directly to the console. 
+ * @param[in]   socket   The client's socket
+ * @return      void
+ */
 void *client_recevie(void *socket){
     long clientS=(long)socket;
     int client_socket =(int)clientS;
     char serveur_reponse[100] ={0};
     while(1){
         int reponse_size=recv((int)client_socket,serveur_reponse,100,0);
-        serveur_reponse[reponse_size] = '\0';  // 确保字符串正确地结束
-        // 如果收到服务器关闭的特殊消息，就退出
+        serveur_reponse[reponse_size] = '\0';  
+        /*receive a message "SERVER_EXIT", exit*/
         if (strcmp(serveur_reponse, "SERVER_EXIT") == 0) {
             printf("Server is closing. Exiting client.\n");
             close(client_socket);
             exit(0);
         }
-        // 如果在写模式，把消息存储到待显示消息中；否则，直接显示消息
+        /*the received messages are stored in a pending messages buffer,otherwise, they are printed directly to the console. */
         if (edit_only_mode) {
             strncpy(pendingMessages[pendingMessagesCount++], serveur_reponse, MESSAGE_SIZE-1);
             pendingMessages[pendingMessagesCount-1][MESSAGE_SIZE-1] = '\0';  // 确保字符串正确地结束
         } else {
             printf("%s\n",serveur_reponse);
             
-        }
-        
+        }      
         memset(serveur_reponse, 0, sizeof(serveur_reponse));
     }
-    pthread_exit(NULL);
-    
+    pthread_exit(NULL);  
+}
+
+/**
+ * @function:   sigintHandler·
+ * @brief       Handles the SIGINT signal ctrl+c
+ *              ctrl +c -> enter edit_only_mode      
+ * @param[in]   sig_num   The signal number
+ * @return      void
+ */
+void handle_sigint(int sig) {
+   edit_only_mode = !edit_only_mode;  
+    if (edit_only_mode) {
+        printf("-----Edit only mode (ctrl+c)--------\nenter your message: \n");
+    } 
 }
 
 
--- serveur.c
+++ serveur.c
@@ -1,11 +1,12 @@
 /********************************************************
-File name : serveur.c
-Author : Ningxin YE, Yiqing CHEN, Yang YANG
-Class : L3 MIAGE(université paris Dauphine_PSL)
-Day : 2023/5/15
-Compile command line : gcc serveur.c -pthread -o serveur
-The command line for running the program ：
-For stop the programme : ctrl +z（）or ctrl + c
+* File name : serveur.c
+* Version 6
+* Author : Ningxin YE, Yiqing CHEN, Yang YANG
+* L3 MIAGE(université paris Dauphine_PSL)
+* Date : 2023/5/15
+* Compile command : gcc serveur.c -pthread -o serveur
+* Command to run the program ：./serveur
+* Terminate the program: ctrl + c
 *********************************************************/
 #include <stdio.h>
 #include <stdlib.h>
@@ -17,52 +18,49 @@ For stop the programme : ctrl +z（）or ctrl + c
 #include <sys/un.h> 
 #include <arpa/inet.h>
 #include <signal.h> 
-#define NUM_MAX 10
 
-int creat_socket();
-int accept_client(int sev_socket);
-void *se_connecter_client(void *arg);
-void send_message_to_other(long id, char *serveur_reponse, char *sperotor);
-void first_connect(long id, char *client_reponse, char *serveur_reponse);
-void delet_client_information(long id);
-int give_time(char *buf);
-void sigintHandler(int sig_num);
-void write_message_in_doc();
+#define NUM_MAX 10 /*Maximum number of clients*/
+#define MAX_MESSAGES 1000 //*Maximum number of messages a client can send*/
+#define MAX_MESSAGE_LENGTH 100 /*Maximum length of one client‘s message*/
 
-pthread_t threads[NUM_MAX];
-pthread_mutex_t waitMemset;
-#define MAX_MESSAGES 1000
-#define MAX_MESSAGE_LENGTH 100
+pthread_t threads[NUM_MAX]; /*One client occupies one thread*/
+pthread_mutex_t waitMemset; /*protect access to the buffer (serveur_reponse)*/
 
-
+/*This structure denotes a client, 
+comprising its name, socket, 
+and an array of messages sent during the program's runtime,
+which are logged to messages.txt upon server exit.*/
 typedef struct
 {
     char name[100];
     int client_socket;
     char messages[MAX_MESSAGES][MAX_MESSAGE_LENGTH];
     int message_count;
-     // Etape 2 : for the gestion of message for a client
 } CLIENT;
-
 CLIENT clients[NUM_MAX] = {0};
 
-char ask_name[30] = "what's your name?";
+/* Declaration of functions */
+int creat_socket();
+int accept_client(int sev_socket);
+void *se_connecter_client(void *arg);
+void send_message_to_other(long id, char *serveur_reponse, char *sperotor);
+void first_connect(long id, char *client_reponse, char *serveur_reponse);
+void delet_client_information(long id);
+void give_time(char *buf);
+void sigintHandler(int sig_num);
+void write_message_in_doc();
 
 int main(int argc, char const *argv[])
 {
-    // 添加信号处理
     signal(SIGINT, sigintHandler);
-    /*Create socket*/
     int sev_socket = creat_socket();
     int client_socket;
     pthread_mutex_init(&waitMemset,NULL);
-    /*Accepter les connexions des clients*/
     while (1)
     {
         client_socket = accept_client(sev_socket);
         printf("***********************************\n");
     }
-    // printf("all the message");
     // close the socket
     for (int i = 0; i < NUM_MAX; i++)
     {
@@ -75,34 +73,37 @@ int main(int argc, char const *argv[])
     return 0;
 }
 
+
+/**
+* @function:        creat_socket
+* @brief            Create a unix serveur socket
+* @return           >0 a file descriptor pointing to socket 
+*                   <0 fail 
+*/
 int creat_socket()
 {
     unlink("Mysock");
-    // Initialisation struct addr serveur
-    struct sockaddr_un addr ={0}; // Address Family UNIX
-    memset(&addr, 0, sizeof(struct sockaddr_un)); // Chemin vers fichier socket local
+    /*Initialisation struct addr serveur*/
+    struct sockaddr_un addr ={0}; 
+    memset(&addr, 0, sizeof(struct sockaddr_un)); 
     addr.sun_family = AF_UNIX;	
     strcpy(addr.sun_path, "./MySock");	
-
-    // Creation socket serveur
+    /*Creation socket serveur*/
     int sev_socket = -1;
     sev_socket = socket(AF_UNIX, SOCK_STREAM, 0);
     if (sev_socket < 0)
     {
         perror("Socket creation error!\n");
-        exit(1);
+        exit(-1);
     }
-
-    
-    // Connection de la socket a l'addresse serveur
+    /*Connection de la socket a l'addresse serveur*/
     int test_bind = bind(sev_socket, (struct sockaddr *)&addr, sizeof(addr));
-
     if (test_bind < 0)
     {
         perror("Binding error!\n");
         exit(-1);
     }
-    // Socket en attente de connexion (a l'ecoute),
+    /* Socket en attente de connexion (a l'ecoute)*/
     int test_listen = listen(sev_socket, NUM_MAX);
     if (test_listen < 0)
     {
@@ -113,9 +114,15 @@ int creat_socket()
     return sev_socket;
 }
 
-int accept_client(int sev_socket)
+/**
+* @fuction:     accept_client
+* @brief        Accepts a connection from a client 
+* @param[in]    sev_socket   The server's socket file descriptor
+* @return       >0 a file descriptor pointing to socket or -1 fail 
+*/
+int accept_client(int   sev_socket)
 {
-    // Initialisation struct addr client
+    //*Initialisation struct addr client*/
     struct sockaddr_un client_addr = {0};
     socklen_t addrlen = sizeof(client_addr);
     printf("Waiting for client connection...\n");
@@ -125,8 +132,7 @@ int accept_client(int sev_socket)
         perror("Error when accepting client connection");
         exit(-1);
     }
-
-    // Find an available slot in the clients array
+    /*Find an available slot in the clients array*/
     int client_index = -1;
     for (int i = 0; i < NUM_MAX; i++)
     {
@@ -136,13 +142,11 @@ int accept_client(int sev_socket)
             break;
         }
     }
-
     if (client_index != -1)
     {
         clients[client_index].client_socket = client_socket;
         pthread_create(&threads[client_index], NULL, se_connecter_client, (void *)(intptr_t)client_index);
         pthread_detach(threads[client_index]);
-        //printf("Connected with client at address: %s\n", inet_ntoa(client_addr.sin_addr));
     }
     else
     {
@@ -153,6 +157,16 @@ int accept_client(int sev_socket)
     return client_socket;
 }
 
+/**
+*@function:     se_connecter_client
+*@brief         Manages communication with a connected client.
+*               it assigns a name to the client
+*               handles messages and disconnections. 
+*               appends a timestamp and client's name to messages,
+*               broadcasts them, 
+*s              tores them in the client's array
+*@return null
+*/
 void *se_connecter_client(void *arg)
 {
     long id = (long)arg;
@@ -162,7 +176,7 @@ void *se_connecter_client(void *arg)
     {
         char client_reponse[100] = {0};
         char serveur_reponse[100] = {0};
-            // if the client hasn't a name(the fist time of connexion)
+            /*the client hasn't a name(the fist time of connexion)*/
             if (strlen(clients[id].name) == 0)
             {
                 first_connect(id, client_reponse, serveur_reponse);
@@ -172,26 +186,25 @@ void *se_connecter_client(void *arg)
             }
             read_size = recv(clients[id].client_socket, client_reponse, 100, 0);
             client_reponse[read_size] = '\0';
-            // this client disconnected or erro when read o
+            /*this client disconnected or erro when read o*/
             if (read_size <= 0||strncmp(client_reponse, "quit", 4) == 0)
             {
                 printf("%s(ID = %ld) disconnected\n", clients[id].name,id);
-                sprintf(serveur_reponse,"%s disconnected\n",clients[id].name);
+                sprintf(serveur_reponse,"%s(ID = %ld) disconnected\n",clients[id].name,id);
                 send_message_to_other(id,serveur_reponse,sperotor);
                 delet_client_information(id);
                 pthread_exit(NULL);
                 break;
             }
-            // the read_size is not 0
+            /*normal situation*/
             else
             {
                 char messageTime[100] = {0};
                 give_time(messageTime);
                 printf("Message received from clien %s(ID=%ld) : %s \n ",clients[id].name,id, client_reponse);
-                sprintf(serveur_reponse, "%s %s : %s", messageTime, clients[id].name, client_reponse);
+                sprintf(serveur_reponse, "%s %s(ID=%ld) : %s", messageTime, clients[id].name, id,client_reponse);
                 send_message_to_other(id,serveur_reponse,sperotor);
-                
-                //将传送的message存入client的数组中
+                /*store the transmitted messages in the client structure's messages array for later output to a file*/
                 if (clients[id].message_count < MAX_MESSAGES) {
                 strncpy(clients[id].messages[clients[id].message_count], client_reponse, MAX_MESSAGE_LENGTH);
                 clients[id].message_count++;
@@ -202,7 +215,14 @@ void *se_connecter_client(void *arg)
     pthread_exit(NULL);
 }
 
-/*用于将消息发送给其他clients*/
+/**
+* @function:    send_message_to_other
+* @brief        Accepts a connection from a client 
+* @param[in]    id               The client's id who sent the message
+* @param[in]    serveur_reponse  The server's response message to be broadcasted
+* @param[in]    sperotor         A separator string, can be NULL
+* @return       null
+*/
 void send_message_to_other(long id, char *serveur_reponse, char *sperotor)
 {
     pthread_mutex_lock(&waitMemset);
@@ -211,8 +231,8 @@ void send_message_to_other(long id, char *serveur_reponse, char *sperotor)
     {
         if (i != id && clients[i].client_socket != 0)
         {
-             char message_to_send[1000];  // 创建一个新的缓冲区
-            strcpy(message_to_send, serveur_reponse);  // 复制要发送的消息到新的缓冲区
+             char message_to_send[1000];  
+            strcpy(message_to_send, serveur_reponse); 
 
             if(sperotor!=NULL){ send(clients[i].client_socket, sperotor, strlen(sperotor), 0);}
             send(clients[i].client_socket, message_to_send, strlen(message_to_send), 0);
@@ -223,9 +243,18 @@ void send_message_to_other(long id, char *serveur_reponse, char *sperotor)
     
 }
 
-/*用于第一次连接的时候分配一个名字*/
+/**
+ * @function:    first_connect
+ * @brief       Assigns a name to a client during their first connection
+ *              It asks the client for their name
+ * @param[in]   id                The client's id
+ * @param[out]  client_reponse    Buffer to store the client's response
+ * @param[out]  serveur_reponse   Buffer to store the server's response
+ * @return      null
+ */
 void first_connect(long id, char *client_reponse, char *serveur_reponse)
 {
+    char ask_name[30] = "what's your name?";
     send(clients[id].client_socket, ask_name, strlen(ask_name), 0);
     int read_size = recv(clients[id].client_socket, client_reponse, 100, 0);
     client_reponse[read_size] = '\0';
@@ -239,13 +268,24 @@ void first_connect(long id, char *client_reponse, char *serveur_reponse)
     send_message_to_other(id, serveur_reponse,NULL);
 }
 
-/*用于删除一个client*/
+/**
+ * @function:   delet_client_information
+ * @brief       Delete a client       
+ * @param[in]   id               The client's id
+ * @return      null
+ */
 void delet_client_information(long id){
     clients[id].client_socket = 0;
     memset(&clients[id].name, '\0', sizeof(clients[id].name));   
 }
 
-int give_time(char *buf)
+/**
+ * @function:   give_time
+ * @brief       Generate current time  
+ * @param[out]  buf             Buffer to store the current time
+ * @return      null
+ */
+void give_time(char *buf)
 {
     time_t t = 0;
     struct tm *tt = NULL;
@@ -255,9 +295,16 @@ int give_time(char *buf)
             tt->tm_hour,
             tt->tm_min, tt->tm_sec);
     buf[strlen(buf)-1]=0;
-    return 0;
+   
 }
 
+/**
+ * @function:   write_message_in_doc
+ * @brief       Writes all client messages into a document
+ *              This function opens a file called "messages.txt" for writing. 
+ *              It iterates over all connected clients and writes each client's name followed by all their messages into the file. 
+ * @return      void
+ */
 void write_message_in_doc(){
     FILE *file = fopen("messages.txt", "w");
     if (file == NULL) {
@@ -278,10 +325,18 @@ void write_message_in_doc(){
     fclose(file);
 }
 
+/**
+ * @function:   sigintHandler·
+ * @brief       Handles the SIGINT signal ctrl+c)
+ *              After the server has exited (ctrl+c), 
+ *              send a message to get all connected clients to exit.
+ *              The messageris stored in the client are written to messages.txt
+ * @param[in]   sig_num   The signal number
+ * @return      void
+ */
 void sigintHandler(int sig_num)
 {
     write_message_in_doc();
-    // 向所有客户端发送一个表示服务器关闭的特殊消息
     for (int i = 0; i < NUM_MAX; i++) {
         if (clients[i].client_socket != 0) {
             write(clients[i].client_socket, "SERVER_EXIT" , strlen("SERVER_EXIT"));
